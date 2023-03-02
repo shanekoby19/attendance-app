b65a934f4c9cf27fca38982e6370babc98807fb3
Shane Kobylecky <skobylecky1@DESKTOP-N059GK8.ClarkCoHS.local>
2023-03-01 16:11:21 -0800

    change the text in App.js and right a small test for App.js

---
+++ src/components/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders hello world in our app', () => {
+    // Create the virtual DOM by rendering our application.
+    render(<App />);
+
+    const h1 = screen.getByText(/hello world/i);
+    expect(h1).toBeInTheDocument();
+})
