2096a725c9d10b5336dab4635eb846bff0f35191
Shane Kobylecky <skobylecky1@gmail.com>
2023-03-02 23:40:26 -0800

    reset our testing suite

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world in our app', () => {
-    // Create the virtual DOM by rendering our application.
-    render(<App />);
+test('this will work', () => {
 
-    const h1 = screen.getByText(/hello world/i);
-    expect(h1).toBeInTheDocument();
-})
-
+});
