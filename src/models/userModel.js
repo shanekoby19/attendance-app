586e90b8d3ed84586306ebb3a0f2cdd8fa361cf9
Shane Kobylecky <skobylecky1@gmail.com>
2023-03-10 12:50:07 -0800

    Create the user model

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    firstName: {
+        type: 'String',
+        required: [true, 'A user must have a first name.'],
+        trim: true,
+    },
+    lastName: {
+        type: 'String',
+        required: [true, 'A user must have a first name.'],
+        trim: true,
+    },
+    email: {
+        type: 'String',
+        required: [true, 'A user must have an email.']
+    },
+    password: {
+        type: 'String',
+        required: [true, 'A user must have a password.']
+    },
+    role: {
+        type: 'String',
+        required: [true, 'A user must have a role.'],
+        enum: {
+            values: ['viewer', 'admin', 'super admin'],
+            message: 'The user role must be one of the following values: viewer, admin, super admin'
+        }
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
